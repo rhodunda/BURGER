5b4c5323a3a0394452d2c0c2f01a6e5e4de32b51
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-09 15:12:12 -0400

    added sign in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,7 +37,8 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
-        }
+        },
+        isSignUp: true
     }
 
     checkValidity(value, rules) {
@@ -86,7 +87,13 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return{isSignUp: !prevState.isSignUp}
+        })
     }
 
     render () {
@@ -116,6 +123,10 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+                <Button 
+                clicked={this.switchAuthModeHandler}
+                btnType='Danger'>Switch to {this.state.isSignUp ? 'Sign in' : 'Sign up'}
+                </Button>
             </div>
         );
     }
@@ -123,7 +134,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     };
 };
 
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,11 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initailState = {
+     
+}
+
+const reducer = (state, action) => {
+
+}
+
+export default reducer
