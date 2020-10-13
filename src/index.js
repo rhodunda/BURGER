b816f7b914c775d526243b7aa4d970fd03985cf0
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-13 18:45:11 -0400

    added the ability to disable redux dev tools

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth'
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// inable to block Redux Dev tools
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
